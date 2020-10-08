956a822c88a5ca9d1a27c279d6d7365e2966e746
stepin105190 <69082292+stepin105190@users.noreply.github.com>
2020-10-08 18:20:02 +0530

    Create number_conversion.h

---
+++ inc/number_conversion.h
@@ -0,0 +1,15 @@
+#ifndef __CONVERSION_H__
+	#define __CONVERSION_H__
+	long int Bin_to_Dec(long int);
+	long int Bin_to_Oct(long int);
+	long int Bin_to_Hex(long int);
+	long int Dec_to_Bin(long int);
+	long int Dec_to_Oct(long int);
+	long int Dec_to_Hex(long int);
+	long int Oct_to_Bin(long int);
+	long int Oct_to_Dec(long int);
+	long int Oct_to_Hex(long int);
+	void Hex_to_Bin(char []);
+    void Hex_to_Dec(char []);
+    void Hex_to_Oct(char []);
+	#endif
